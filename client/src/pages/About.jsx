9e106ef21f3d3676dbd4cea4bf2c64d6208ba5e7
Hamza <Hamza_mohammed15@hotmail.com>
2024-04-19 06:30:05 +0100

    added in filter comboboxes in tutor profile page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import aboutImage from "../assets/about-us-1.jpg";
+import aboutImage2 from "../assets/about-us-2.jpg";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="py-16">
+      <h2 className="text-3xl font-bold text-center mb-4">
+        Our Vision and Mission
+      </h2>
+      <div className="w-full flex items-center mb-8">
+        <div className="md:flex items-center">
+          <img
+            src={aboutImage2}
+            alt="Our Vision"
+            className="mb-4 rounded-lg mr-4 md:ml-4"
+          />
+          <div>
+            <p className="text-gray-700">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+              accusamus voluptatum a ratione fuga deserunt nihil voluptatibus
+              dolorem accusantium voluptates ipsam laboriosam eligendi eum
+              repudiandae, perspiciatis dignissimos ut corrupti quas.
+            </p>
+            <p className="text-gray-700">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+              accusamus voluptatum a ratione fuga deserunt nihil voluptatibus
+              dolorem accusantium voluptates ipsam laboriosam eligendi eum
+              repudiandae, perspiciatis dignissimos ut corrupti quas.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- client/src/pages/TutorProfile.jsx
+++ client/src/pages/TutorProfile.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import SearchAndFilter from "../components/features/SearchAndFilter";
 
 const TutorProfile = () => {
   const [tutors, setTutors] = useState([]);
@@ -9,7 +10,7 @@ const TutorProfile = () => {
   useEffect(() => {
     const fetchTutors = async () => {
       try {
-        const response = await axios.get('http://localhost:7777/tutors');
+        const response = await axios.get("http://localhost:7777/tutors");
         setTutors(response.data);
         setLoading(false);
       } catch (error) {
@@ -31,24 +32,54 @@ const TutorProfile = () => {
 
   return (
     <div className="container mx-auto px-4">
-      <h2 className="text-2xl font-bold mb-6 mt-12">Tutors</h2>
+      <div className="flex">
+        <select className="mr-4 mt-4 px-4 border-gray-400 rounded" id="Subjects">
+          <option value="" disabled selected>
+            Select a Subject...
+          </option>
+          <option value="English">English</option>
+          <option value="Maths">Maths</option>
+          <option value="Science">Science</option>
+          <option value="ICT">ICT</option>
+        </select>
+        <select className="mr-4 mt-4 px-4 border-gray-400 rounded" id="Levels">
+          <option value="" disabled selected>
+            All Levels...
+          </option>
+          <option value="English">Primary</option>
+          <option value="Maths">KS3</option>
+          <option value="Maths">GCSE</option>
+          <option value="Science">A-Level</option>
+          <option value="Science">Degree</option>
+        </select>
+        <SearchAndFilter />
+      </div>
+      <h2 className="text-2xl font-bold mb-6 mt-10">Tutors</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {tutors.map((tutor) => (
-          <div key={tutor._id} className="bg-white rounded-lg shadow-md p-4 flex items-center">
-            <img src={tutor.avatarUrl} alt="Avatar" className="w-32 h-32 rounded-full mr-4" />
+          <div
+            key={tutor._id}
+            className="bg-white rounded-lg shadow-md p-4 flex items-center"
+          >
+            <img
+              src={tutor.avatarUrl}
+              alt="Avatar"
+              className="w-32 h-32 rounded-full mr-4"
+            />
             <div>
               <div className="font-semibold"> {tutor.username}</div>
               <div className="mt-2">
                 <span className="font-semibold">Bio:</span> {tutor.profile.bio}
               </div>
               <div className="mt-2">
-                <span className="font-semibold">Hourly Rate:</span> $ {tutor.profile.hourlyRate}
+                <span className="font-semibold">Hourly Rate:</span> ${" "}
+                {tutor.profile.hourlyRate}
               </div>
               <div className="mt-3">
-              <button class="bg-green-500 hover:bg-green-600 text-xs text-white font-bold py-2 px-3 rounded">
-                Availability
-              </button>
-            </div>
+                <button class="bg-green-500 hover:bg-green-600 text-xs text-white font-bold py-2 px-3 rounded">
+                  Availability
+                </button>
+              </div>
             </div>
           </div>
         ))}
